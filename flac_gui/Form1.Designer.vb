3f325806ab86239ecd68c8fa3fc76ffb02c36139
moisesmcardona <moises@moises-studios.com>
2018-07-04 11:24:12 -0400

    Add project files.

---
+++ flac_gui/Form1.Designer.vb
@@ -0,0 +1,191 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.InputTxt = New System.Windows.Forms.TextBox()
+        Me.OutputTxt = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.InputBrowseBtn = New System.Windows.Forms.Button()
+        Me.OutputBrowseBtn = New System.Windows.Forms.Button()
+        Me.CompressionLevelChoose = New System.Windows.Forms.ComboBox()
+        Me.StartBtn = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.SuspendLayout
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = true
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(228, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Step 1: Browse for Input folder with FLAC Files:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = true
+        Me.Label2.Location = New System.Drawing.Point(12, 52)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(269, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Step 2: Browse for output folder for encoded FLAC files:"
+        '
+        'InputTxt
+        '
+        Me.InputTxt.Location = New System.Drawing.Point(15, 26)
+        Me.InputTxt.Name = "InputTxt"
+        Me.InputTxt.Size = New System.Drawing.Size(225, 20)
+        Me.InputTxt.TabIndex = 2
+        '
+        'OutputTxt
+        '
+        Me.OutputTxt.Location = New System.Drawing.Point(15, 68)
+        Me.OutputTxt.Name = "OutputTxt"
+        Me.OutputTxt.Size = New System.Drawing.Size(225, 20)
+        Me.OutputTxt.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = true
+        Me.Label3.Location = New System.Drawing.Point(12, 101)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(99, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Compression Level:"
+        '
+        'InputBrowseBtn
+        '
+        Me.InputBrowseBtn.Location = New System.Drawing.Point(246, 24)
+        Me.InputBrowseBtn.Name = "InputBrowseBtn"
+        Me.InputBrowseBtn.Size = New System.Drawing.Size(75, 23)
+        Me.InputBrowseBtn.TabIndex = 5
+        Me.InputBrowseBtn.Text = "Browse"
+        Me.InputBrowseBtn.UseVisualStyleBackColor = true
+        '
+        'OutputBrowseBtn
+        '
+        Me.OutputBrowseBtn.Location = New System.Drawing.Point(246, 68)
+        Me.OutputBrowseBtn.Name = "OutputBrowseBtn"
+        Me.OutputBrowseBtn.Size = New System.Drawing.Size(75, 23)
+        Me.OutputBrowseBtn.TabIndex = 6
+        Me.OutputBrowseBtn.Text = "Browse"
+        Me.OutputBrowseBtn.UseVisualStyleBackColor = true
+        '
+        'CompressionLevelChoose
+        '
+        Me.CompressionLevelChoose.FormattingEnabled = true
+        Me.CompressionLevelChoose.Items.AddRange(New Object() {"0", "1", "2", "3", "4", "5", "6", "7", "8"})
+        Me.CompressionLevelChoose.Location = New System.Drawing.Point(15, 117)
+        Me.CompressionLevelChoose.Name = "CompressionLevelChoose"
+        Me.CompressionLevelChoose.Size = New System.Drawing.Size(96, 21)
+        Me.CompressionLevelChoose.TabIndex = 7
+        '
+        'StartBtn
+        '
+        Me.StartBtn.Location = New System.Drawing.Point(117, 101)
+        Me.StartBtn.Name = "StartBtn"
+        Me.StartBtn.Size = New System.Drawing.Size(204, 37)
+        Me.StartBtn.TabIndex = 8
+        Me.StartBtn.Text = "Start"
+        Me.StartBtn.UseVisualStyleBackColor = true
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = true
+        Me.Label4.Location = New System.Drawing.Point(12, 148)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(51, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Progress:"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(15, 165)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(306, 23)
+        Me.ProgressBar1.Step = 1
+        Me.ProgressBar1.TabIndex = 10
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = true
+        Me.Label5.Location = New System.Drawing.Point(12, 201)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(119, 13)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "GUI by Moises Cardona"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = true
+        Me.Label6.Location = New System.Drawing.Point(293, 201)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(28, 13)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "v1.0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(333, 224)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.StartBtn)
+        Me.Controls.Add(Me.CompressionLevelChoose)
+        Me.Controls.Add(Me.OutputBrowseBtn)
+        Me.Controls.Add(Me.InputBrowseBtn)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.OutputTxt)
+        Me.Controls.Add(Me.InputTxt)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.MaximizeBox = false
+        Me.Name = "Form1"
+        Me.Text = "FLAC GUI"
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents InputTxt As TextBox
+    Friend WithEvents OutputTxt As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents InputBrowseBtn As Button
+    Friend WithEvents OutputBrowseBtn As Button
+    Friend WithEvents CompressionLevelChoose As ComboBox
+    Friend WithEvents StartBtn As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+End Class
---
+++ flac_gui/Form1.vb
@@ -0,0 +1,52 @@
+﻿Public Class Form1
+
+    Private Sub InputBrowseBtn_Click(sender As Object, e As EventArgs) Handles InputBrowseBtn.Click
+        Dim InputBrowser As New FolderBrowserDialog
+        InputBrowser.ShowNewFolderButton = False
+        Dim OkAction As MsgBoxResult = InputBrowser.ShowDialog
+        If OkAction = MsgBoxResult.Ok Then
+            InputTxt.Text = InputBrowser.SelectedPath
+        End If
+    End Sub
+
+    Private Sub OutputBrowseBtn_Click(sender As Object, e As EventArgs) Handles OutputBrowseBtn.Click
+        Dim OutputBrowser As New FolderBrowserDialog
+        OutputBrowser.ShowNewFolderButton = True
+        Dim OkAction As MsgBoxResult = OutputBrowser.ShowDialog
+        If OkAction = MsgBoxResult.Ok Then
+            OutputTxt.Text = OutputBrowser.SelectedPath
+        End If
+    End Sub
+
+    Private Sub StartBtn_Click(sender As Object, e As EventArgs) Handles StartBtn.Click
+        If Not My.Computer.FileSystem.DirectoryExists(OutputTxt.Text) Then My.Computer.FileSystem.CreateDirectory(OutputTxt.Text)
+        ProgressBar1.Maximum = My.Computer.FileSystem.GetFiles(InputTxt.Text).Count
+        ProgressBar1.Value = 0
+        For Each File In IO.Directory.GetFiles(InputTxt.Text)
+            run_flac(File, OutputTxt.Text + "\" + My.Computer.FileSystem.GetName(File), CompressionLevelChoose.SelectedItem)
+            ProgressBar1.PerformStep()
+            Me.Update()
+        Next
+        MsgBox("Finished")
+    End Sub
+    Private Sub run_flac(Input_File As String, Output_File As String, CompressionLevel As String)
+        Dim flacProcessInfo As New ProcessStartInfo
+        Dim flacProcess As Process
+        flacProcessInfo.FileName = "flac.exe"
+        flacProcessInfo.Arguments = "-" & CompressionLevel & " -V """ + Input_File + """ -o """ + Output_File + """"
+        flacProcessInfo.CreateNoWindow = True
+        flacProcessInfo.RedirectStandardOutput = True
+        flacProcessInfo.UseShellExecute = False
+        flacProcess = Process.Start(flacProcessInfo)
+        flacProcess.WaitForExit()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CompressionLevelChoose.SelectedIndex = My.Settings.CompressionLevel
+    End Sub
+
+    Private Sub CompressionLevelChoose_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CompressionLevelChoose.SelectedIndexChanged
+        My.Settings.CompressionLevel = CompressionLevelChoose.SelectedIndex
+        My.Settings.Save()
+    End Sub
+End Class
